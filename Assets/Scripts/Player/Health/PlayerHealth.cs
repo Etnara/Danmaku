5f1c98396c89f69df8a379d9639290c854299b59
Etnara <51210840+Etnara@users.noreply.github.com>
2022-10-28 21:42:59 -0500

    I-Frame Blinking Animation

--- Assets/Scripts/Player/Health/PlayerHealth.cs
+++ Assets/Scripts/Player/Health/PlayerHealth.cs
@@ -9,19 +9,33 @@ public class PlayerHealth : MonoBehaviour{
     float damageTime;
     bool hit;
     public float DBTime;
+    public float iTime;
 
     public PlayerBombs Bomb;
+    public SpriteRenderer playerRenderer;
+
+    float t;
+    bool increasing;
 
     void Awake(){
         currentHealth = startingHealth;
+        t = 0.0f;
     }
 
     void OnTriggerEnter2D(Collider2D other){
 
-        if (other.tag == "Damage" || other.tag == "Enemy"){
+        
+            
 
-            // Deathbombing var
-            damageTime = Time.time;
+        if (other.tag == "Damage" || other.tag == "Enemy"){
+            
+            // Invincible
+            if (damageTime > 0 && Time.time < damageTime + iTime){
+                return;
+            }
+
+            // Deathbombing and Iframe var
+            damageTime = Time.time;                         //Debug.Log(damageTime);
             hit = true;
 
             if (currentHealth > 0)
@@ -38,6 +52,7 @@ public class PlayerHealth : MonoBehaviour{
     }
 
     void Update(){
+        // Deathbomb
         if(hit)
             if (Time.time < damageTime + DBTime){
                 if (Bomb.dbble){
@@ -49,4 +64,34 @@ public class PlayerHealth : MonoBehaviour{
             else
                 hit = false;
     }
+
+    void FixedUpdate() {
+    
+         // iFrame Animation
+        if (damageTime > 0 && Time.time < damageTime + iTime) {
+            
+            // Increasing or Decreasing
+            if (t >= 0.99)
+                increasing = false;
+            if (t <= 0.01)
+                increasing = true;
+
+            // +- t
+            if (increasing)
+                t += 1/50f;
+            else
+                t -= 1/50f;
+
+            // Lerp
+            playerRenderer.GetComponent<SpriteRenderer>().color = new Color (255, 0, 0, t);
+            // Debug.Log(t);
+                    
+        } else
+            // Correction because floating point rounding is dumb
+            playerRenderer.GetComponent<SpriteRenderer>().color = new Color (255, 0, 0, 1);
+ 
+
+    
+    }
+
 }
