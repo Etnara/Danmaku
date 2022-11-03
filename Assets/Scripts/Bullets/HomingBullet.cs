3f6128f5f9caa4f1cd3a8fac01e59130d506aa21
Etnara <zakbutt18@gmail.com>
2022-11-02 19:28:57 -0500

    Homing Bullet Script

---
+++ Assets/Scripts/Bullets/HomingBullet.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class HomingBullet : MonoBehaviour
+{
+
+    public Transform target;
+
+    public float speed;
+    public float rotateSpeed;
+
+    private Rigidbody2D rb;
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate() {
+        
+        Vector2 direction = (Vector2)target.position - rb.position;
+        
+        direction.Normalize();
+
+        float rotateAmount = Vector3.Cross(direction, transform.up).z;
+        rb.angularVelocity = -rotateAmount * rotateSpeed;
+        
+        Debug.Log(-rotateAmount * rotateSpeed);
+        Debug.Log(rb.angularVelocity);
+        rb.velocity = transform.up * speed;
+    }
+}
