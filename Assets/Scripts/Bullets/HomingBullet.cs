a0251636638dcab4f7543a7650fecda55204cfcb
Etnara <51210840+Etnara@users.noreply.github.com>
2022-11-04 10:26:35 -0500

    Added Shot Level 2 Prefabs & Updated Homing Script

--- Assets/Scripts/Bullets/HomingBullet.cs
+++ Assets/Scripts/Bullets/HomingBullet.cs
@@ -4,30 +4,49 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
-public class HomingBullet : MonoBehaviour
-{
+public class HomingBullet : MonoBehaviour {
 
-    public Transform target;
-
+    private Transform _target;
+    private Rigidbody2D _rb;
+    
     public float speed;
     public float rotateSpeed;
 
-    private Rigidbody2D rb;
-    void Start() {
-        rb = GetComponent<Rigidbody2D>();
+    private void Start() {
+        _target = FindClosestEnemy() == null ? null: FindClosestEnemy().transform;
+        _rb = GetComponent<Rigidbody2D>();
     }
 
     private void FixedUpdate() {
+        _rb.velocity = transform.up * speed;
+
+        if (!_target) {
+            _rb.angularVelocity = 0;
+            return; 
+        }
         
-        Vector2 direction = (Vector2)target.position - rb.position;
+        Vector2 direction = (Vector2)_target.position - _rb.position;
         
         direction.Normalize();
 
         float rotateAmount = Vector3.Cross(direction, transform.up).z;
-        rb.angularVelocity = -rotateAmount * rotateSpeed;
-        
-        Debug.Log(-rotateAmount * rotateSpeed);
-        Debug.Log(rb.angularVelocity);
-        rb.velocity = transform.up * speed;
+        _rb.angularVelocity = -rotateAmount * rotateSpeed;
+    }
+
+    private GameObject FindClosestEnemy() {
+        GameObject[] gos;
+        gos = GameObject.FindGameObjectsWithTag("Enemy");
+        GameObject closest = null;
+        float distance = Mathf.Infinity;
+        Vector3 position = transform.position;
+        foreach (GameObject go in gos) {
+            Vector3 diff = go.transform.position - position;
+            float curDistance = diff.sqrMagnitude;
+            if (curDistance < distance) {
+                closest = go;
+                distance = curDistance;
+            }
+        }
+        return closest;
     }
 }
